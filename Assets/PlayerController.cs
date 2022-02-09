27e58d936c16e0e87427c26eb81547f6213b8c33
ulichovick <ulichovick@gmail.com>
2022-02-08 21:49:45 -0500

    now, the gun and the player rotate towards the mouse

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -11,6 +11,8 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] Transform WeaponsArm;
 
+    [SerializeField] Transform Player;
+
     private Camera mainCamera;
     void Start()
     {
@@ -26,13 +28,27 @@ public class PlayerController : MonoBehaviour
         playerRigidBody.velocity = movementInput * movementSpeed;
 
         Vector3 mousePosition = Input.mousePosition;
-        Vector3 screenPoint = mainCamera.WorldToScreenPoint(transform.localPosition);
+        Vector3 screenPoint = mainCamera.WorldToScreenPoint(Player.localPosition);
 
         Vector2 offset = new Vector2(mousePosition.x - screenPoint.x, mousePosition.y - screenPoint.y);
 
         float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
 
         WeaponsArm.rotation = Quaternion.Euler(0, 0, angle);
+
+        if (mousePosition.x < screenPoint.x)
+        {
+            Player.localScale = new Vector3(-1f, 1f, 1f);
+            WeaponsArm.localScale = new Vector3(-1f, -1f, 1f);
+        }
+        else if(mousePosition.x > screenPoint.y)
+        {
+            //Player.localScale = new Vector3(1f, 1f, 0);
+            Player.localScale = Vector3.one;
+            //WeaponsArm.localScale = new Vector3(1f, 1f, 0);
+            WeaponsArm.localScale = Vector3.one;
+
+        }
 
     }
 }
