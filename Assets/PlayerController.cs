c668e81fbcca845634185a2c9743c6f789519b44
ulichovick <ulichovick@gmail.com>
2022-02-10 00:02:00 -0500

    added player animations(idle, walking and torpedo), deleted arm and shotgun bcuz fuck that shitgit add -Agit add -A

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,19 +4,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
     [SerializeField] int movementSpeed;
     [SerializeField] Rigidbody2D playerRigidBody;
     private Vector2 movementInput;
-
-    [SerializeField] Transform WeaponsArm;
-
+    //[SerializeField] Transform WeaponsArm;
     [SerializeField] Transform Player;
-
     private Camera mainCamera;
+    private float torpedo;
+    private Animator playerAnimator;
+    // Start is called before the first frame update
     void Start()
     {
         mainCamera = Camera.main;
+
+        playerAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -24,6 +25,7 @@ public class PlayerController : MonoBehaviour
     {
         movementInput.x = Input.GetAxisRaw("Horizontal");
         movementInput.y = Input.GetAxisRaw("Vertical");
+        torpedo = Input.GetAxisRaw("Fire2");
 
         playerRigidBody.velocity = movementInput * movementSpeed;
 
@@ -34,19 +36,45 @@ public class PlayerController : MonoBehaviour
 
         float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
 
-        WeaponsArm.rotation = Quaternion.Euler(0, 0, angle);
+        //WeaponsArm.rotation = Quaternion.Euler(0, 0, angle);
 
-        if (mousePosition.x < screenPoint.x)
+        if (movementInput.x < 0)
         {
             Player.localScale = new Vector3(-1f, 1f, 1f);
-            WeaponsArm.localScale = new Vector3(-1f, -1f, 1f);
+            //WeaponsArm.localScale = new Vector3(-1f, -1f, 1f);
         }
-        else if(mousePosition.x > screenPoint.y)
+        else if (movementInput.x > 0)
         {
             //Player.localScale = new Vector3(1f, 1f, 0);
             Player.localScale = Vector3.one;
             //WeaponsArm.localScale = new Vector3(1f, 1f, 0);
-            WeaponsArm.localScale = Vector3.one;
+            //WeaponsArm.localScale = Vector3.one;
+
+        }
+
+
+        if (movementInput != Vector2.zero)
+        {
+            playerAnimator.SetBool("isWalking", true);
+        }
+        else
+        {
+            playerAnimator.SetBool("isWalking", false);
+        }
+
+
+        if (movementInput != Vector2.zero & torpedo == 1)
+        {
+            //Player.position += new Vector3(torpedo, 0f, 0f);
+            //Player.position += new Vector3(torpedo*(movementSpeed*2), 0f, 0f);
+            playerRigidBody.velocity = new Vector2(torpedo * movementInput.x, 0) * (movementSpeed * 3);
+            Debug.Log(playerRigidBody.velocity);
+            playerAnimator.SetBool("isFlipping", true);
+
+        }
+        else
+        {
+            playerAnimator.SetBool("isFlipping", false);
 
         }
 
