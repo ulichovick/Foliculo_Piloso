c41a1913c9c42ccb74317affc12ae5f6620453f9
ulichovick <ulichovick@gmail.com>
2022-02-15 00:02:32 -0500

    added firing animation and bullet animation and colliders

---
+++ Assets/Scripts/PlayerBulletController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBulletController : MonoBehaviour
+{
+    [SerializeField] float bulletSpeed = 5f;
+    private Rigidbody2D bulletRigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bulletRigidBody.velocity = transform.right * bulletSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        Destroy(gameObject);
+    }
+}
